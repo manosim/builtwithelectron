a580d6aff29b3d3483f57bbc4a9c60edffb95780
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-10-22 20:42:22 +0100

    DRF Settings & SubmitEntrySerializer

--- project/settings.py
+++ project/settings.py
@@ -126,9 +126,12 @@ USE_TZ = True
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
 }
 
 
