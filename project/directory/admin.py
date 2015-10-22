f555da0cbe195c9935445548e2f28f9505c51a3d
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-10-22 18:43:16 +0100

    Admin for Entries & Tags

--- project/directory/admin.py
+++ project/directory/admin.py
@@ -3,23 +3,31 @@ from project.directory.models import Entry, Tag
 
 
 class EntriesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'short_description', 'author', 'get_tags', 'cover', 'website_url', 'repo_url', 'description',)
+    list_display = ('name', 'short_description', 'author', 'get_tags', 'cover', 'website_url', 'repo_url', 'has_description',)
 
     fieldsets = (
-        (None, {'fields': ('name', 'short_description',)}),
-        ('Personal info', {'fields': ('author',)}),
-        ('Permissions', {'fields': ('cover', 'thumbnail_cover', 'website_url', 'repo_url', 'description',)}),
+        ('Required Information', {'fields': ('name', 'short_description', 'author', 'website_url', )}),
+        ('Additional Information', {'fields': ('cover', 'thumbnail_cover', 'tags', 'repo_url', 'description',)}),
     )
 
     readonly_fields = ('thumbnail_cover',)
 
     def get_tags(self, obj):
         return "\n".join([p.name for p in obj.tags.all()])
+    get_tags.short_description = 'Tags'
+
+    def has_description(self, obj):
+        return obj.description != ""
+    has_description.short_description = 'Desc.'
+    has_description.boolean = True
 
 
 class TagsAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_no_apps', 'created', 'modified',)
 
+    def get_no_apps(self, obj):
+        return Entry.objects.filter(tags=obj).count()
+    get_no_apps.short_description = 'No. of Apps'
 
 admin.site.register(Entry, EntriesAdmin)
 admin.site.register(Tag, TagsAdmin)
