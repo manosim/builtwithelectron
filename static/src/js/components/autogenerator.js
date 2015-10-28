e2103bc1f9bfaebf953f7636ba7f71a6f9246d69
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-10-28 21:11:29 +0000

    Hello Reloading

--- static/src/js/components/autogenerator.js
+++ static/src/js/components/autogenerator.js
@@ -2,6 +2,7 @@ var _ = require('underscore');
 var React = require('react');
 var Select = require('react-select');
 var Dropzone = require('react-dropzone');
+var Loading = require('reloading');
 var apiRequests = require('../utils/api-requests');
 
 var Autogenerator =  require('./autogenerator');
@@ -11,8 +12,8 @@ var SubmitForm = React.createClass({
   getInitialState: function () {
     return {
       url: 'http://www.github.com/ekonstantinidis/gitify',
-      submitDisabled: false,
-      loading: false
+      loading: false,
+      submitDisabled: false
     };
   },
 
@@ -31,23 +32,25 @@ var SubmitForm = React.createClass({
     console.log(owner, '/', repo);
 
     this.setState({
-      submitDisabled: true
+      loading: true,
+      submitDisabled: true,
     });
 
     apiRequests
       .get('https://api.github.com/repos/' + owner + '/' + repo)
       .end(function (err, response) {
         if (response && response.ok) {
+          setTimeout(function() {
           self.setState({
-            success: true,
-            errors: [],
+            errors: false,
             loading: false,
             submitDisabled: false
           });
           self.props.gotDetails(response.body);
+        }, 75000);
         } else {
           self.setState({
-            errors: response.body,
+            errors: true,
             loading: false,
             submitDisabled: false
           });
@@ -58,7 +61,10 @@ var SubmitForm = React.createClass({
   render: function () {
     return (
       <div className='autogenerator'>
-        <h3>Open-Source Populator</h3>
+        <h3>Generate from GitHub Repository</h3>
+        <Loading className="loading" shouldShow={this.state.loading}>
+          <h3>Grabbing your repository details from <i className='fa fa-github' /></h3>
+        </Loading>
         <form className='form'>
           <div className='form-group'>
             <input type='text' id='name' className='form-control input-lg' value={this.state.url} onChange={this.handleChange.bind(this, 'url')} />
