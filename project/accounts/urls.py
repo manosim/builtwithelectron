b208fb74cc1f057aa20c55cd75c7d0d87bf08637
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-10-29 20:13:59 +0000

    Initialise ProfileView

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
-from project.accounts.views import OAuthCallbackView
+from project.accounts.views import OAuthCallbackView, ProfileView
 
 urlpatterns = patterns('',
 
     url(r'^oauth-callback/$', OAuthCallbackView.as_view(), name="oauth-callback"),
+    url(r'^profile/$', ProfileView.as_view(), name="profile"),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'directory:home'}, name='logout'),
 
 )
