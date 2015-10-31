1e4bfa2e0fa0d0d1918b9b8647c01ac85d0e11ed
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-10-31 01:36:06 +0100

    Get media url

--- project/settings.py
+++ project/settings.py
@@ -78,6 +78,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
