6bde9ab2296c03921eaaa1d960b85565b7baca29
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-11-05 20:48:11 +0000

    Tags with SlugField

--- project/directory/models.py
+++ project/directory/models.py
@@ -16,7 +16,7 @@ class Tag(models.Model):
     created = models.DateTimeField(auto_now_add=True, db_index=True)
     modified = models.DateTimeField(auto_now=True)
 
-    name = models.CharField(max_length=255, unique=True)
+    name = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
